e5152e31edda29fefb654f1169670b85d477fd1e
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 18:19:43 +0100

    using mount -o bind

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ echo ">> copying files"
 #ln -s /usr/config/proxy-server/letsencrypt-live /etc/letsencrypt/live 2> /dev/null > /dev/null 
 #cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 2> /dev/null > /dev/null
 #cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null 
-ln -s /usr/config/proxy-server/letsencrypt /etc/letsencrypt
-cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 
-cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/  
+mount -o bind /usr/config/proxy-server/letsencrypt /etc/letsencrypt
+mount -o bind /usr/config/proxy-server/sites/links /etc/apache2/sites-available 
+mount -o bind /usr/config/proxy-server/sites/links /etc/apache2/sites-enabled  
 exit 0
 # exec CMD
 #echo ">> exec docker CMD"
