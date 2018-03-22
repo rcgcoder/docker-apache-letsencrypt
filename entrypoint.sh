c051d31e5079e3f91dc24d01f19f1f78f768c615
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 07:37:36 +0100

    changing return of entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo ">> copying files"
 ln -s /usr/config/proxy-server/letsencrypt-live /etc/letsencrypt/live 2> /dev/null > /dev/null 
 cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 2> /dev/null > /dev/null
 cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null 
-
+exit 0
 # exec CMD
 #echo ">> exec docker CMD"
 #echo "$@"
