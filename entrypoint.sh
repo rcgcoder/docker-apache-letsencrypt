5bdf8ab33d0ab860885828a9657f674d277e8b98
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 22:33:07 +0100

    only link to sites enabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ echo ">> copying files"
 service apache2 stop
 rm -r /etc/letsencrypt
 ln -s /usr/config/proxy-server/letsencrypt /etc 2> /dev/null > /dev/null 
-rm -r /etc/apache2
-ln -s /usr/config/proxy-server/apache-config /etc 2> /dev/null > /dev/null 
+rm -r /etc/apache2/sites-enabled
+ln -s /usr/config/proxy-server/apache-config/sites-enabled /etc/apache2/sites-enabled 2> /dev/null > /dev/null 
 rm -r /var/www/html
 ln -s /usr/config/proxy-server/apache-contents /var/www/html 2> /dev/null > /dev/null 
 
