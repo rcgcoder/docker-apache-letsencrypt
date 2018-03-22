fbc3d13542235946b7695b18da4e7af43555da11
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 21:32:50 +0100

    correcting errors on ln -s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo ">> copying files"
-rmdir -r /etc/letsencrypt
-ln -s /usr/config/proxy-server/letsencrypt /etc/letsencrypt 2> /dev/null > /dev/null 
-rmdir -r /etc/apache2
-ln -s /usr/config/proxy-server/apache-config /etc/apache2 2> /dev/null > /dev/null 
 service apache2 stop
+rmdir -r /etc/letsencrypt
+ln -s /usr/config/proxy-server/letsencrypt /etc 2> /dev/null > /dev/null 
+rmdir -r /etc/apache2
+ln -s /usr/config/proxy-server/apache-config /etc 2> /dev/null > /dev/null 
 service apache2 start
 
 exit 0
