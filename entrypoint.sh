a8b72d79c3b20ff2768217995ab2353da12de0de
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 18:10:33 +0100

    link to whole letsencrypt path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo ">> copying files"
 #ln -s /usr/config/proxy-server/letsencrypt-live /etc/letsencrypt/live 2> /dev/null > /dev/null 
 #cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 2> /dev/null > /dev/null
 #cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null 
-ln -s /usr/config/proxy-server/letsencrypt-live /etc/letsencrypt/live 
+ln -s /usr/config/proxy-server/letsencrypt /etc/letsencrypt
 cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 
 cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/  
 exit 0
