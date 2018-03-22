4e391d82b2cb72784151ae922edcbff7423be675
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 21:38:43 +0100

    error with rmdir ---> rm -r

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo ">> copying files"
 service apache2 stop
-rmdir -r /etc/letsencrypt
+rm -r /etc/letsencrypt
 ln -s /usr/config/proxy-server/letsencrypt /etc 2> /dev/null > /dev/null 
-rmdir -r /etc/apache2
+rm -r /etc/apache2
 ln -s /usr/config/proxy-server/apache-config /etc 2> /dev/null > /dev/null 
+rm -r /var/www/html
+ln -s /usr/config/proxy-server/apache-contents /var/www/html 2> /dev/null > /dev/null 
+
 service apache2 start
 
 exit 0
