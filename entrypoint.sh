80e2bb1265968a2192d202be1b5b980e0209c379
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-22 07:11:00 +0100

    with symlinks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-mkdir -p /etc/letsencrypt/live 
-cp /usr/config/proxy-server/certs/letsencrypt/live/* /etc/letsencrypt/live/ 2> /dev/null > /dev/null 
-cp /usr/config/proxy-server/apache-config/sites-available/* /etc/apache2/sites-available/ 2> /dev/null > /dev/null
-cp /usr/config/proxy-server/apache-config/sites-available/* /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null 
+ln -s /usr/config/proxy-server/letsencrypt-live /etc/letsencrypt/live 2> /dev/null > /dev/null 
+cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-available/ 2> /dev/null > /dev/null
+cp /usr/config/proxy-server/sites/links/* /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null 
 
 # exec CMD
 echo ">> exec docker CMD"
