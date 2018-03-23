8c0802effb11ad79d81bedef17ecc6fed32486cf
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-23 06:07:58 +0100

    using mounted dns configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo ">> copying files"
 service apache2 stop
+/etc/init.d/nscd stop
+/etc/init.d/bind9 stop
 rm -r /etc/letsencrypt
 ln -s /usr/config/proxy-server/letsencrypt /etc 2> /dev/null > /dev/null 
 rm -r /etc/apache2/sites-enabled
 ln -s /usr/config/proxy-server/apache-config/sites-enabled /etc/apache2/sites-enabled 2> /dev/null > /dev/null 
 rm -r /var/www/html
 ln -s /usr/config/proxy-server/apache-contents /var/www/html 2> /dev/null > /dev/null 
-
+rm -r /etc/bind
+ln -s /usr/config/dns-server/bind/etc /etc/bind 2> /dev/null > /dev/null 
+/etc/init.d/bind9 start
+/etc/init.d/nscd start
 service apache2 start
 
 exit 0
